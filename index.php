31fd47fdcc5a037f40323afb035e353bb49f4710
komenAnna <komenriana@gmail.com>
2024-03-21 11:39:44 +0300

    set a function to accept status codes and display coreesponding views

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require "functions.php";
 // get the current URI
 $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
+
 // if ($uri === '/'){
 //     require 'controllers/index.php';
 // } else if ($uri === '/about') {
@@ -16,16 +17,24 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 // };
 // refactor above to an array to make it easier to add new routes in the future
 $routes = [
-    '/' =>  require 'controllers/index.php',
-    '/about' =>  require 'controllers/about.php',
-    '/projects' =>  require 'controllers/projects.php',
-    '/contact' =>  require 'controllers/contact.php',
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/projects' => 'controllers/projects.php',
+    '/contact' => 'controllers/contact.php'
 ];
+
+// he function looks for the code that corresponds the http response status and dispalys that view if it exists
+// however, the default is set to 404 ie if the error is 422, it will override 404 then display the corresponding view
+function abort($code = 404){
+    http_response_code($code);
+    require "views/{$code}.php";
+    die();
+}
+
 // check if the key in the array exists in the uri
 if (array_key_exists($uri, $routes)){
     require $routes[$uri];
 } else {
-    // if not dispaly a 404 page
-    http_response_code(404);
-    require 'views/404.php';
+    abort();
+    
 };
