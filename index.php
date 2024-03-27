5867e7849debcf6c0acdc6a252c833aebdebeb62
komenAnna <komenriana@gmail.com>
2024-03-27 11:41:29 +0300

    [feat]: Database connection

--- index.php
+++ index.php
@@ -2,39 +2,9 @@
 
 require "functions.php";
 
-// get the current URI
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+require 'router.php';
 
-
-// if ($uri === '/'){
-//     require 'controllers/index.php';
-// } else if ($uri === '/about') {
-//     require 'controllers/about.php';
-// } else if ($uri === '/projects') {
-//     require 'controllers/projects.php';
-// } else if ($uri === '/contact') {
-//     require 'controllers/contact.php';
-// };
-// refactor above to an array to make it easier to add new routes in the future
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/projects' => 'controllers/projects.php',
-    '/contact' => 'controllers/contact.php'
-];
-
-// he function looks for the code that corresponds the http response status and dispalys that view if it exists
-// however, the default is set to 404 ie if the error is 422, it will override 404 then display the corresponding view
-function abort($code = 404){
-    http_response_code($code);
-    require "views/{$code}.php";
-    die();
-}
-
-// check if the key in the array exists in the uri
-if (array_key_exists($uri, $routes)){
-    require $routes[$uri];
-} else {
-    abort();
-    
-};
+require 'Database.php'
+// Connection to database
+$db = new Database()
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
